b19829d9b87f32a142e1973dd700c8306ac7e4b5
Kinga Bohus <kingabohus@Kingas-MacBook-Pro.local>
2019-06-20 12:37:04 +0200

    First commit

---
+++ engine.py
@@ -0,0 +1,106 @@
+import tcod as libtcod
+
+from components.fighter import Fighter
+from entity import Entity, get_blocking_entities_at_location
+from input_handlers import handle_keys
+from map_objects.game_map import GameMap
+from game_states import GameStates
+from render_functions import clear_all, render_all
+from fov_functions import initialize_fov, recompute_fov
+
+
+def main():
+    screen_width = 80
+    screen_height = 50
+    map_width = 80
+    map_height = 45
+
+    room_max_size = 16
+    room_min_size = 8
+    max_rooms = 20
+
+    fov_algorithm = 0
+    fov_light_walls = True
+    fov_radius = 8
+
+    max_monsters_per_room = 3
+
+    colors = {
+        "dark_wall": libtcod.Color(50,53,51),
+        'dark_ground': libtcod.Color(78,96,83),
+        'light_wall': libtcod.Color(87,87,87),
+        'light_ground': libtcod.Color(113,175,199)
+    }
+
+    fighter_component = Fighter(hp=30, defense=2, power=5)
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white, 'Player', blocks=True, fighter=fighter_component)
+    entities = [player]
+
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+
+    con = libtcod.console_new(screen_width, screen_height)
+
+    game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room)
+
+    fov_recompute = True
+    fov_map = initialize_fov(game_map)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    game_state = GameStates.PLAYERS_TURN
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+
+        fov_recompute = False
+
+        libtcod.console_flush()
+
+        clear_all(con, entities)
+
+        action = handle_keys(key)
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move and game_state == GameStates.PLAYERS_TURN:
+            dx, dy = move
+            destination_x = player.x + dx
+            destination_y = player.y + dy
+
+            if not game_map.is_blocked(destination_x, destination_y):
+                target = get_blocking_entities_at_location(entities, destination_x, destination_y)
+
+                if target:
+                    print('You kick the ' + target.name + ' in the shins, much to its annoyance!')
+                else:
+                    player.move(dx, dy)
+
+                    fov_recompute = True
+                game_state = GameStates.ENEMY_TURN
+
+        if exit:
+            return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+        if game_state == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity.ai:
+                    entity.ai.take_turn(player, fov_map, game_map, entities)
+
+            game_state = GameStates.PLAYERS_TURN
+
+
+if __name__ == '__main__':
+    main()
